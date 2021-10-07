56258449d0d1d59d4b56038608525417a22d3454
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2021-10-07 16:33:50 +0530

    LonLat settings

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function init(){
   const map = new ol.Map({
     view: new ol.View({
       projection: 'EPSG:4326',
-      center: [80.998158, 6.825878],
+      center: [80.998158, 6.825878], // remeber to set latlon in 'lon' first and 'lat' scecond
       zoom: 12
     }),
     layers: [
