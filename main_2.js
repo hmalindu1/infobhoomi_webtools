b3b5cd76335ed27f1d412aad74ffb204c110446d
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2021-10-18 17:17:47 +0530

    Commented extents and added new Tile sources

--- main_2.js
+++ main_2.js
@@ -7,15 +7,15 @@ function init() {
       projection: 'EPSG:4326',
       center: [80.998158, 6.825878], // remeber to set latlon in 'lon' first and 'lat' scecond
       zoom: 12,
-      extent: [79.60507728159328, 5.87518664626196, 81.9021271806201, 9.873768101066211]
+      // extent: [79.60507728159328, 5.87518664626196, 81.9021271806201, 9.873768101066211]
     }),
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM(),
         zIndex: 1,
-        visible: true,
-        extent:[79.60507728159328, 5.87518664626196, 81.9021271806201, 9.873768101066211],
-        opacity:1
+        visible: false,
+        // extent:[79.60507728159328, 5.87518664626196, 81.9021271806201, 9.873768101066211],
+        opacity: 1
       })
     ],
     target: 'js-map',
@@ -28,16 +28,36 @@ function init() {
           url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         }),
         zIndex: 0,
-        visible: true,
-        extent: [79.60507728159328, 5.87518664626196, 81.9021271806201, 9.873768101066211],
+        visible: false,
+        // extent: [79.60507728159328, 5.87518664626196, 81.9021271806201, 9.873768101066211],
         opacity: 1
+      }),
+      // Bing Maps Basemap Layer
+      new ol.layer.Tile({
+        source: new ol.source.BingMaps({
+          key: "AshrP2YvBPN60emxJEFYNNNtBYcUAqEJ2J0FctgznIkRgrNnOdbPdRpbht_X7eD8",
+          imagerySet: 'AerialWithLabels',
+        }),
+        zIndex: 2,
+        visible: false,
       })
     ]
   });
 
   map.addLayer(layer_group);
 
-  map.on('click', function(e) {
+/* map.on('click', function(e) {
     console.log(e.coordinate);
-  })
+  })*/
+
+  // CartoDB Basemap Layer
+  const cartoDB_base_layer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: 'https://{1-4}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{scale}.png'
+    }),
+    visible: true
+  });
+
+  map.addLayer(cartoDB_base_layer);
+
 }
